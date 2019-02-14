4789176a6e5f651799a3b8c28ba7d26d8f835be6
beresnev <beresnev@protei.ru>
2019-02-14 18:40:17 +0300

    Initial commit

---
+++ src/main/java/ru/loaltyplant/movierater/Main.java
@@ -0,0 +1,8 @@
+package ru.loaltyplant.movierater;
+
+public class Main {
+    public static void main(String[] args) {
+        var hello = "Hello, Java 11!";
+        System.out.println(hello);
+    }
+}
